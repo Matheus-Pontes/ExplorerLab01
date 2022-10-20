4fd09ed8b234066a526b235e2cfc121605d9e84a
Matheus-Pontes <pontesm10@outlook.com>
2022-10-19 22:19:45 -0300

    Aula02 - finalizada com expressoes regulares para mascara de cartões

--- src/main.js
+++ src/main.js
@@ -22,13 +22,13 @@ function setCardType(type = "default") {
 setCardType();
 
 const ccCVC = document.querySelector('#security-code');
-const maskCVCOptions = {
+const maskCVCPattern = {
     mask: '0000'
-}
-const maskCVC = IMask(ccCVC, maskCVCOptions);
+};
+const maskCVC = IMask(ccCVC, maskCVCPattern);
 
 const ccExpirationDate = document.querySelector('#expiration-date');
-const maskExpirationDateOptions = {
+const maskExpirationDatePattern = {
     mask: "MM{/}YY",
     blocks: {
         MM: {
@@ -42,8 +42,37 @@ const maskExpirationDateOptions = {
             to: String(new Date().getFullYear() + 10).slice(2)
         }
     }
-}
-const maskExpirationDate = IMask(ccExpirationDate, maskExpirationDateOptions);
+};
+const maskExpirationDate = IMask(ccExpirationDate, maskExpirationDatePattern);
+
+const cardNumber = document.querySelector("#card-number");
+const maskCardNumberPattern = {
+    mask: [
+        {
+            mask: "0000 0000 000 0000",
+            regex: /^4\d{0,15}/,
+            cardType: "visa",
+
+        },
+        {
+            mask: "0000 0000 000 0000",
+            regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
+            cardType: "mastercard",
+        },
+        {
+            mask: "0000 0000 000 0000",
+            cardType: "default",
+        }
+    ],
+    dispatch: function(appended, dynamicMasked) {
+        const number = (dynamicMasked.value + appended).replace(/\D/g, "");
+
+        const foundMask = dynamicMasked.compiledMasks.find(({regex}) => number.match(regex));
+
+        return foundMask;
+    }
+};
+const maskCardNumber = IMask(cardNumber, maskCardNumberPattern);
 
 // Deixar acessível na DOM pelo DEVTOOLS
 // globalThis.setCardType = setCardType;
